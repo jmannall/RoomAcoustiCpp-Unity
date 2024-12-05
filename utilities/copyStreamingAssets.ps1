f4b463394c4ef7201ac4f71969f6a4b851872716
jmannall <110119245+jmannall@users.noreply.github.com>
2024-12-05 15:12:26 +0000

    Copy meta files

--- utilities/copyStreamingAssets.ps1
+++ utilities/copyStreamingAssets.ps1
@@ -11,21 +11,25 @@ Write-Host "Source Directory: $sourceDir"
 
 # Define the folder to copy (replace 'YourFolder' with your folder name)
 $folderToCopy = "$sourceDir\StreamingAssets"
+$folderToCopyMeta = "$sourceDir\StreamingAssets.meta"
 
 # Define the destination directory (one level above the source directory)
 $destinationDir = Split-Path -Path $sourceDir -Parent
 
 # Define the destination path in the parent directory
 $destinationPath = "$destinationDir\StreamingAssets"
+$destinationPathMeta = "$destinationDir\StreamingAssets.meta"
 
 # Check if the folder exists
 if (Test-Path $folderToCopy) {
     # Copy the folder to the parent directory
     Copy-Item -Path $folderToCopy -Destination $destinationPath -Recurse -Force
+    Copy-Item -Path $folderToCopyMeta -Destination $destinationPathMeta -Recurse -Force
     Write-Host "Folder copied successfully to: $destinationPath"
 
     # Delete the original folder from the current directory
     Remove-Item -Path $folderToCopy -Recurse -Force
+    Remove-Item -Path $folderToCopyMeta -Recurse -Force
     Write-Host "Original folder deleted from: $folderToCopy"
 } else {
     Write-Host "The folder $folderToCopy does not exist."
