a861c119f6fac6aa90443b020ea7e3dc02a4aca2
Matteo-Scerbo <matteo.scerbo@hotmail.it>
2025-11-05 11:33:36 +0000

    Click and drag on minimap works

--- Scripts/UI/MiniMapController.cs
+++ Scripts/UI/MiniMapController.cs
@@ -1,8 +1,9 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(RectTransform))]
-public class MiniMapController : MonoBehaviour, IPointerClickHandler
+public class MiniMapController : MonoBehaviour
 {
     /*
      * Code adapted from
@@ -16,8 +17,11 @@ public class MiniMapController : MonoBehaviour, IPointerClickHandler
 
     [SerializeField, Range(1, 1000)]
     public float pixelsPerMeter;
+    [SerializeField, Range(0, 1000)]
+    public int pixelMargin;
 
     private RACAudioSource[] racSources;
+    private int selectedSourceIdx = -1;
 
     void Start()
     {
@@ -31,27 +35,42 @@ public class MiniMapController : MonoBehaviour, IPointerClickHandler
 
     void Update()
     {
+        bool cursorInBounds = false;
         if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
             mapRectTransform, Input.mousePosition, mapEventCamera, out screenCursorPos))
         {
-            if (!RectTransformUtility.RectangleContainsScreenPoint(mapRectTransform,
-                Input.mousePosition, mapEventCamera))
-                return;
-
-            screenCursorPos.x -= mapRectTransform.rect.x;
-            screenCursorPos.y -= mapRectTransform.rect.y;
+            screenCursorPos.x -= mapRectTransform.rect.x + pixelMargin;
+            screenCursorPos.y -= mapRectTransform.rect.y + pixelMargin;
 
             worldCursorPos = screenCursorPos / pixelsPerMeter;
 
-            //Debug.Log($"Pointer: {worldCursorPos} ({Input.GetMouseButton(0)})");
+            if (worldCursorPos.x > 0 && worldCursorPos.x < 21
+                && worldCursorPos.y > 0 && worldCursorPos.y < 26)
+                cursorInBounds = true;
         }
-        else
-            return;
-        
-        if (!Input.GetMouseButton(0))
+
+        if (!cursorInBounds)
         {
+            // The cursor is OOB. Make sure that no source label is underlined.
+            TextMeshProUGUI[] sourceLabel;
+            for (int i = 0; i < racSources.Length; i++)
+            {
+                sourceLabel = racSources[i].gameObject.GetComponentsInChildren<TextMeshProUGUI>();
+
+                foreach (TextMeshProUGUI l in sourceLabel)
+                    l.fontStyle = FontStyles.Bold;
+            }
+            // If a source was being dragged, forget about it.
+            selectedSourceIdx = -1;
+            // Stop to avoid dragging a source OOB.
+            return;
+        }
+
+        if (selectedSourceIdx < 0)
+        {
+            // At the previous frame, the mouse button was NOT down; we need to update the selected source.
+            // Detect the closest source, and underline its label.
             float minSourceDist = float.MaxValue;
-            int selectedSourceIdx = -1;
             for (int i = 0; i < racSources.Length; i++)
             {
                 Vector2 sourcePos;
@@ -68,30 +87,41 @@ public class MiniMapController : MonoBehaviour, IPointerClickHandler
             }
 
             if (selectedSourceIdx < 0)
+            {
+                Debug.LogWarning("Failed to detect closest source.");
                 return;
+            }
 
-            string sourceName = racSources[selectedSourceIdx].gameObject.name;
-            Debug.Log($"Closest source: {sourceName}");
+            TextMeshProUGUI[] sourceLabel;
+            for (int i = 0; i < racSources.Length; i++)
+            {
+                sourceLabel = racSources[i].gameObject.GetComponentsInChildren<TextMeshProUGUI>();
+
+                foreach (TextMeshProUGUI l in sourceLabel)
+                {
+                if (i == selectedSourceIdx)
+                    l.fontStyle = FontStyles.Bold | FontStyles.Underline;
+                else
+                    l.fontStyle = FontStyles.Bold;
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            // If the mouse button IS down, update the position of the source being dragged.
+            Vector3 draggedSourcePosition;
+            draggedSourcePosition.x = worldCursorPos.x;
+            draggedSourcePosition.z = worldCursorPos.y;
+            draggedSourcePosition.y = racSources[selectedSourceIdx].transform.position.y;
+            Quaternion draggedSourceRotation = racSources[selectedSourceIdx].transform.rotation;
+            racSources[selectedSourceIdx].transform.SetPositionAndRotation(draggedSourcePosition, draggedSourceRotation);
         }
         else
-            Debug.Log("Clicking...");
-    }
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(mapRectTransform,
-            eventData.pressPosition, eventData.pressEventCamera, out screenCursorPos))
         {
-            if (!RectTransformUtility.RectangleContainsScreenPoint(mapRectTransform,
-                Input.mousePosition, eventData.pressEventCamera))
-                return;
-
-            screenCursorPos.x -= mapRectTransform.rect.x;
-            screenCursorPos.y -= mapRectTransform.rect.y;
-
-            worldCursorPos = screenCursorPos / pixelsPerMeter;
-
-            Debug.Log($"Clicked position: {worldCursorPos}");
+            // If the mouse button is NOT down, forget the selected source. It needs to be refreshed at the next frame.
+            selectedSourceIdx = -1;
+            // By contrast, if the mouse button IS down, the selected source will be the same at the next frame.
         }
     }
 }
